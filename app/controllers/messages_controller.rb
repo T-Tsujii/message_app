7a576c89b4b1b407c8663bbef9e4b412251a8586
T.Tsujii <ttakehisabt@gmail.com>
2020-02-16 20:40:52 +0900

    新規投稿ページまでを作成

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,32 @@
+class MessagesController < ApplicationController
+  # ログイン必須とする
+  before_action :authenticate_user!
+
+  def index
+    @messages = Message.includes(:user)
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    current_user.messages.create!(message_params)
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
